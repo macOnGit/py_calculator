066cfd198385fdba0afc47a4de2b8423135c1d5d
cmac <>
2024-06-14 16:47:47 -0400

    specified python3 in tests

--- test_calculator.py
+++ test_calculator.py
@@ -3,29 +3,26 @@ import subprocess
 
 
 class TestCalculator(unittest.TestCase):
+    cmd = ["python3", "calculator.py"]
 
     def test_add_nums(self):
         completed = subprocess.run(
-            ["python", "calculator.py", "2", "3"], capture_output=True, text=True
+            self.cmd + ["2", "3"], capture_output=True, text=True
         )
         self.assertEqual(completed.stdout, "6")
 
     def test_bad_input(self):
         completed = subprocess.run(
-            ["python", "calculator.py", "pizza", "3"], capture_output=True, text=True
+            self.cmd + ["str", "3"], capture_output=True, text=True
         )
         self.assertEqual(completed.stdout, "")
 
     def test_no_input(self):
-        completed = subprocess.run(
-            ["python", "calculator.py"], capture_output=True, text=True
-        )
+        completed = subprocess.run(self.cmd, capture_output=True, text=True)
         self.assertEqual(completed.stdout, "")
 
     def test_not_enough_args(self):
-        completed = subprocess.run(
-            ["python", "calculator.py", "4"], capture_output=True, text=True
-        )
+        completed = subprocess.run(self.cmd, capture_output=True, text=True)
         self.assertEqual(completed.stdout, "")
 
 
